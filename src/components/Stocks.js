d0d1fe91083a4c1f9c58f6094359fee0d7c62931
Tushar Jain <tushar.jain@getvymo.com>
2022-11-23 04:37:56 +0530

    Modified stock list data format to implement search on top of it

--- src/components/Stocks.js
+++ src/components/Stocks.js
@@ -8,37 +8,55 @@ export default function Stocks() {
   useEffect(() => {
     axios.get("https://prototype.sbulltech.com/api/v2/instruments")
       .then(res => {
-        let data = res.data.split("\n").map(str => str.split(","));
+        let data = res.data.trim()
+                          .split("\n")
+                          .map(str => str.split(","));
+        let fields = data[0];
+        data = data.slice(1).reduce((json, row) => {
+          let rowObj = {};
+          fields.forEach((field, index) => {
+            rowObj[field] = row[index];
+          })
+          return json.concat(rowObj)
+        }, []);
         setData(data);
       })
   }, [])
 
+  const searchRow = (value) => {
+    let matchingData = [];
+    // data.slice(1).forEach((row) => {
+    //   if()
+    // })
+  }
+
   let body, header;
   if (data.length) {
-    header = <tr>{data[0].map(column => <th>{column}</th>)}</tr>;
-    body = data.slice(1)
-      .reduce((allRows, row) => {
-        allRows.push(
-            <tr>
-              {row.map((column, index) => {
-                  if(index === 0) {
-                    return (
-                        <td>
-                          <Link to={`/quotes/${column}`}>
-                            {column}
-                          </Link>
-                        </td>
-                      )
-                  }
-                  return (<td>{column}</td>);
-                })}
-            </tr>
-        );
-        return allRows;
-      }, []);
+    header = <tr>{Object.keys(data[0]).map(column => <th key={column}>{column}</th>)}</tr>;
+    body = data.map((row) => (
+      <tr key={row.Symbol}>
+        {Object.values(row).map((dataPt, index) => {
+          if(index === 0) {
+            return (
+                <td key={row.Symbol + dataPt}>
+                  <Link to={`/quotes/${dataPt}`}>
+                    {dataPt}
+                  </Link>
+                </td>
+              )
+          }
+          return (<td key={row.Symbol + dataPt}>{dataPt}</td>);
+        })}
+      </tr>
+    ))
   }
   return (
     <div>
+      <input
+        type="search"
+        onChange={e => searchRow(e.target.value)}
+        placeholder={"Search..."}
+      />
       <table>
         <thead>
           {header}
