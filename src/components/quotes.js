eff219b4303d5903e3ce9ab6efe6a9294c24d10b
Tushar Jain <tushar.jain@getvymo.com>
2022-11-23 02:56:00 +0530

    Fix search feature

--- src/components/quotes.js
+++ src/components/quotes.js
@@ -1,43 +1,32 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import _ from "lodash";
 
 export default function Quotes() {
-    console.log("inside quotes");
     const [data, setData] = useState([]);
     const [order, setOrder] = useState("asc");
 
     let { stockId } = useParams();
     useEffect(() => {
         axios.get(`https://prototype.sbulltech.com/api/v2/quotes/${stockId}`)
-            .then(res => {console.log("got the data"); reorderData(order, res.data.payload[stockId]);})
+            .then(res => {reorderData(order, res.data.payload[stockId]);})
     }, []);
 
-    const reorderData = (order, data) => {
-        console.log("reorderData", order);
-        if(order === "asc") {
-            console.log('**');
-            setData(data.sort((a, b) => Date.parse(a.time) - Date.parse(b.time)));
-        }
-        else {
-            console.log('##');
-            setData(data.sort((a, b) => Date.parse(b.time) - Date.parse(a.time)));
-        }
-    }
-
     useEffect(() => {
-        console.log("rearrange", order);
         reorderData(order, data);
     }, [order])
 
+    const reorderData = (order, data) => {
+        setData(_.orderBy(data, ['time'], [order]));
+    }
+
     const toggleOrder = () => {
-        console.log("toggleOrder", order);
         setOrder(order === "asc" ? "desc" : "asc");
     }
 
     let header, body;
     if(data.length) {
-        console.log(data);
         header = <tr>{Object.keys(data[0]).map(column => <th key={column}>{column}</th>)}</tr>;
         body = data.map(row => <tr key={row.price + Date.parse(row.time)}>
                                 {Object.values(row).map(val => <td key={val}>{val}</td>)}
